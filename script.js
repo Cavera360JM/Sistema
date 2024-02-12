f28d78b4dbf2e6fbcb759114b8765d6e41df456d
João Miguel <97974646+Cavera360JM@users.noreply.github.com>
2024-02-12 17:05:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const startButton = document.getElementById("startButton");
+    const missionContainer = document.getElementById("missionContainer");
+
+    startButton.addEventListener("click", function() {
+        missionContainer.style.display = "block";
+    });
+
     let level = 1;
     const levelElement = document.getElementById("level");
     const questElement = document.getElementById("quest");
-    
-    // Estrutura de dados para armazenar as missões por dia
+
     const missionsByDay = {
-        1: ["Estudar 2 horas", "Treinar 1 hora", "Ler por 15 minutos"],
-        // Adicione mais missões para outros dias conforme necessário
+        1: ["Treinar 1 hora por dia", "Estudar 2 horas", "Ler por 15 minutos"],
     };
 
     document.getElementById("increaseBtn").addEventListener("click", function() {
@@ -18,18 +23,16 @@ document.addEventListener("DOMContentLoaded", function() {
     function updateQuest(day) {
         const missions = missionsByDay[day] || [];
         if (missions.length > 0) {
-            questElement.innerHTML = missions.map(mission => `<p class="mission">${mission}</p>`).join('');
+            questElement.innerHTML = missions.map(mission => `<p class="mission"><input type="checkbox"> ${mission}</p>`).join('');
         } else {
             questElement.innerHTML = "<p class='mission'>Nenhuma missão disponível</p>";
         }
     }
 
-    // Inicializar o FullCalendar
     const calendarEl = document.getElementById('calendar');
     const calendar = new FullCalendar.Calendar(calendarEl, {
         selectable: true,
         dateClick: function(info) {
-            // Atualizar a quest com base no dia selecionado
             const day = info.date.getDate();
             level = day;
             levelElement.innerText = level;
